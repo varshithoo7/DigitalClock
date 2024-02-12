6e6455e7f8d423ebe34d3a42122fa8e58ff6fc1b
svarshith7 <svarshith@gmail.com>
2024-02-12 12:57:59 -0500

    Changed the Background Color to Yellow

--- Code.py
+++ Code.py
@@ -6,7 +6,7 @@ class DigitalClockHoursMinutes:
         self.root = root
         self.root.title("Digital Clock (Hours and Minutes)")
         self.root.geometry("300x100")
-        self.root.configure(background="white")
+        self.root.configure(background="yellow")
 
         self.time_label = tk.Label(self.root, font=("Helvetica", 24), bg="white")
         self.time_label.pack(pady=20)
