27d2b0eaeb277f54c27746bd5bbe0535149e78a9
svarshith7 <svarshith@gmail.com>
2024-02-14 16:57:12 -0500

    Changed to Pink

--- Code.py
+++ Code.py
@@ -6,7 +6,7 @@ class DigitalClockHoursMinutes:
         self.root = root
         self.root.title("Digital Clock (Hours and Minutes)")
         self.root.geometry("900x500")
-        self.root.configure(background="yellow")
+        self.root.configure(background="pink")
 
         self.time_label = tk.Label(self.root, font=("Helvetica", 24), bg="white")
         self.time_label.pack(pady=20)
